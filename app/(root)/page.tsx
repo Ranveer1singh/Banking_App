c99055b58f9e353f33b44812e8e2283c8c0d941a
Ranveer1singh <ranveersinghtomar2000@gmail.com>
2024-08-17 17:04:05 +0530

    home page ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  return (
-    <div className=''>Home</div>
-  )
-}
+  const loggedIn = { firstName: "Ranveer" };
 
-export default Home
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Acess and manage your Account and transaction effiectly"
+          />
+
+          <TotalBalanceBox 
+          accounts = {[]}
+          totalBanks = {1}
+          totalCurrentBalance = {786.78}
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
