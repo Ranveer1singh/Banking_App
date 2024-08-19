6522a1e046327c64082ad3537de64deebb7da4f1
Ranveer1singh <ranveersinghtomar2000@gmail.com>
2024-08-19 16:16:03 +0530

    sign up and sign in reuseable component

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -18,29 +18,30 @@ import { Input } from "@/components/ui/input";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-
-const formSchema = z.object({
-  email: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-});
+import CustomInput from "./CustomInput";
+import { authFormSchema } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
 
 const AuthForm = ({ type }: { type: string }) => {
   const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof authFormSchema>>({
+    resolver: zodResolver(authFormSchema),
     defaultValues: {
       email: "",
+      password: "",
     },
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof authFormSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
+    setIsLoading(true)
     console.log(values);
+    // setIsLoading(false)
   }
 
   return (
@@ -69,33 +70,89 @@ const AuthForm = ({ type }: { type: string }) => {
         <>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <FormField
+              {type === 'sign-up' && (
+                <>
+                 <CustomInput
+                control={form.control}
+                label="First Name"
+                name="firstName"
+                placeholder="Enter your first name"
+              />
+                 <CustomInput
+                control={form.control}
+                label="Last Name"
+                name="lastName"
+                placeholder="Enter your last name"
+              />
+                 <CustomInput
+                control={form.control}
+                label="Address Name"
+                name="addressName"
+                placeholder="Enter your Address name"
+              />
+                 <CustomInput
+                control={form.control}
+                label="State Name"
+                name="stateName"
+                placeholder="Ex : Mp"
+              />
+                 <CustomInput
+                control={form.control}
+                label="Postal Code"
+                name="postalCode"
+                placeholder="Ex : 461111"
+              />
+                 <CustomInput
+                control={form.control}
+                label="Date of birth"
+                name="dateOfbirth"
+                placeholder="YYYY-MM-DD"
+              />
+                 <CustomInput
+                control={form.control}
+                label="SSN"
+                name="ssn"
+                placeholder="Ex : 123"
+              />
+                </>
+              )}
+              <CustomInput
                 control={form.control}
                 name="email"
-                render={({ field }) => (
-                 <div className="form-item">
-                    <FormLabel className="form-label">
-                        Email
-                    </FormLabel>
-                 <div className="flex w-full flex-col">
-                    <FormControl>
-                        <Input
-                        className="input-class"
-                        placeholder="Enter an Email"
-                        {...field}
-                        />
-                    </FormControl>
-                    <FormMessage  
-                    className="form-message"
-                    / >
-
-                 </div>
-                 </div>
-                )}
+                label="Email"
+                placeholder="Enter your Email"
               />
-              <Button type="submit">Submit</Button>
+              <CustomInput
+                control={form.control}
+                name="password"
+                label="Password"
+                placeholder="Enter Password"
+              />
+              <div className="flex flex-col gap-4">
+              <Button type="submit" className="form-btn" disabled={isLoading}>
+                {isLoading ? (
+                  <>
+                  <Loader2
+                  className="animate-spin"
+                  size={20}
+                  /> &nbsp; Loading....
+                  </>
+                ) : type === 'sign-in' ? 'Sign In' : 'Sign Up'}
+              </Button>
+              </div>
             </form>
           </Form>
+          <footer className="flex justify-center gap-1 ">
+              <p className="text-14 font-normal text-gray-600">
+                {type === "sign-in" ? 
+                'Dont have an account?' : 'Already have a account?'}
+              </p>
+              <Link
+              className="form-link"
+              href={type === 'sign-in' ? '/sign-up' : '/sign-in'}>
+              {type === 'sign-in' ? 'sign-up' : 'sign-in'}
+              </Link>
+          </footer>
         </>
       )}
     </section>
--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,7 @@
 import { type ClassValue, clsx } from "clsx";
 import qs from "query-string";
 import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -193,3 +194,16 @@ export const getTransactionStatus = (date: Date) => {
 
   return date > twoDaysAgo ? "Processing" : "Success";
 };
+
+
+export const authFormSchema = z.object({
+  firstName : z.string().min(3),
+  lastName : z.string().min(3),
+  addressName : z.string().min(50),
+  stateName : z.string().min(2).max(20),
+  postalCode : z.string().min(3).max(6),
+  dateOfbirth : z.string().min(3),
+  ssn : z.string().min(3),
+  email: z.string().email(),
+  password: z.string().min(8),
+});
