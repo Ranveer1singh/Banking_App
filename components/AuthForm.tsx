62289a0348faf3d13d4adc7df7495b3a7382ca66
Ranveer1singh <ranveersinghtomar2000@gmail.com>
2024-08-25 16:54:22 +0530

    plaid set up for bank connection

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -23,6 +23,7 @@ import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { signIn, signUp } from "@/lib/actions/user.action";
+import PlaidLink from "./PlaidLink";
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter(); 
@@ -87,9 +88,11 @@ const AuthForm = ({ type }: { type: string }) => {
           </p>
         </div>
       </header>
-      {user ? (
-        <div className="flex flex-col gap-4">{/* plaidLink */}</div>
-      ) : (
+      {/* {user ? ( */}
+        <div className="flex flex-col gap-4">
+          <PlaidLink user={user} variant="primary"/>
+          </div>
+      {/* ) : ( */}
         <>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -193,7 +196,7 @@ const AuthForm = ({ type }: { type: string }) => {
             </Link>
           </footer>
         </>
-      )}
+      {/* )} */}
     </section>
   );
 };
--- lib/actions/user.action.tsx
+++ lib/actions/user.action.tsx
@@ -6,21 +6,30 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 import { createSessionClient } from "../appwrite";
-import { parseStringify } from "../utils";
+import { encryptId, parseStringify } from "../utils";
+import {
+  CountryCode,
+  ProcessorTokenCreateRequest,
+  ProcessorTokenCreateRequestProcessorEnum,
+  Products,
+} from "plaid";
+import { plaidClient } from "@/lib/plaid";
+import { revalidatePath } from "next/cache";
+import { addFundingSource } from "./dwolla.action";
 
-export const signIn = async ({email , password}:signInProps) => {
+export const signIn = async ({ email, password }: signInProps) => {
   try {
     // Mutation / DataBase / Make Fetch
     const { account } = await createAdminClient();
-    const response = await account.createEmailPasswordSession(email, password)
+    const response = await account.createEmailPasswordSession(email, password);
 
-    return parseStringify(response)
+    return parseStringify(response);
   } catch (error) {
     console.error("error", error);
   }
 };
 export const signUp = async (userData: SignUpParams) => {
-    const {email, password, firstName, lastName} = userData;
+  const { email, password, firstName, lastName } = userData;
   try {
     // Create a User account
     const { account } = await createAdminClient();
@@ -41,7 +50,7 @@ export const signUp = async (userData: SignUpParams) => {
       secure: true,
     });
 
-    return parseStringify(newUserAccount)
+    return parseStringify(newUserAccount);
   } catch (error) {
     console.error("error", error);
   }
@@ -52,21 +61,106 @@ export const signUp = async (userData: SignUpParams) => {
 export async function getLoggedInUser() {
   try {
     const { account } = await createSessionClient();
-    const user =  await account.get();
-    return parseStringify(user)
+    const user = await account.get();
+    return parseStringify(user);
   } catch (error) {
     return null;
   }
 }
 
-export const logoutAccount = async () =>{
+export const logoutAccount = async () => {
   try {
-    const {account} = await createSessionClient();
+    const { account } = await createSessionClient();
 
-    cookies().delete('appwrite-session')
+    cookies().delete("appwrite-session");
 
-    await account.deleteSession('current')
+    await account.deleteSession("current");
+  } catch (error) {}
+};
+
+export const createBankAccount = async ({
+  userId,
+  bankId,
+  accountId,
+  fundingSourceUrl,
+  sharableId,
+}:createBankAccountProps) => {
+  try {
+    const {dataBase} = await createAdminClient();
+
+    const bankAccount = await dataBase.createDocument()
   } catch (error) {
-    
+    console.log("error", error);
   }
-}
+};
+
+export const createLinkToken = async (user: User) => {
+  try {
+    const tokenParams = {
+      user: {
+        client_user_id: user.$id,
+      },
+      client_name: user.name,
+      products: ["auth"] as Products[],
+      language: "en",
+      country_codes: ["US"] as CountryCode[],
+    };
+    const response = await plaidClient.linkTokenCreate(tokenParams);
+    return parseStringify({ linkToken: response.data.link_token });
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+export const exchangePublicToken = async ({ publicToken, user }) => {
+  try {
+    const response = await plaidClient.itemPublicTokenExchange({
+      public_token: publicToken,
+    });
+    const accessToken = response.data.access_token;
+    const itemId = response.data.item_id;
+
+    // GET ACCOUNT INFORMATION FROM PLAID USING THE ACCESS TOKEN
+    const accountResponse = await plaidClient.accountsGet({
+      access_token: accessToken,
+    });
+    const accountData = accountResponse.data.accounts[0];
+
+    // CREATE A PROCESSOR TOKEN FOR DWOLLA USING THE ACCESS TOKEN AND ACCOUNT ID
+    const request: ProcessorTokenCreateRequest = {
+      access_token: accessToken,
+      account_id: accountData.account_id,
+      processor: "dwalla" as ProcessorTokenCreateRequestProcessorEnum,
+    };
+
+    const processorTokenResponse = await plaidClient.processorTokenCreate(
+      request
+    );
+    const processorToken = await processorTokenResponse.data.processor_token;
+
+    // CREATE A FUNFDING SOURCE URL FOR THE ACCOUNT USING THE DWOLLA CUSTOMER ID , PROCESSOR TOKEN AND BANK NAME
+
+    const fundingSourceUrl = await addFundingSource({
+      dwollaCustomerId: user.dwollaCustomerId,
+      processorToken,
+      bankName: accountData.name,
+    });
+
+    if (!fundingSourceUrl) throw Error;
+
+    await createBankAccount({
+      userId: user.$id,
+      bankId: itemId,
+      accountId: accountData.account_id,
+      fundingSourceUrl,
+      sharableId: encryptId(accountData.account_id),
+    });
+    revalidatePath("/");
+
+    return parseStringify({
+      publicTokenExachange: "complete",
+    });
+  } catch (error) {
+    console.log("error", error);
+  }
+};
